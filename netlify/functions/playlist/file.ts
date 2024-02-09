d74cf80a3d20685137d8436453ba9bf1e5dd1d7b
He110te4m <i@he110.info>
2024-02-09 17:46:37 +0800

    feat: update source direction

--- netlify/functions/playlist/file.ts
+++ netlify/functions/playlist/file.ts
@@ -1,10 +1,5 @@
 import glob from 'fast-glob'
-import { resolve } from 'node:path'
 
 export async function getAllPlayList() {
-  console.log({
-    dir: __dirname,
-    cwd: process.cwd(),
-  })
-  return glob('./sources/**/*.m3u?(8)', { absolute: true, onlyFiles: true, cwd: resolve(__dirname, '..') })
+  return glob('./sources/**/*.m3u?(8)', { absolute: true, onlyFiles: true, cwd: __dirname })
 }
