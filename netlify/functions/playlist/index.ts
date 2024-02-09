d6cef1b957dd6e8cabb84c6343b2ead36586aafe
He110te4m <i@he110.info>
2024-02-09 16:58:36 +0800

    refactor: remove export

--- netlify/functions/playlist/index.ts
+++ netlify/functions/playlist/index.ts
@@ -1,8 +1,6 @@
 import { getAllPlayList } from './file'
 import { readPlayList } from './readPlayList'
 
-export * from './writePlayList'
-
 export async function generatePlayList(files?: string[]) {
   const playListFiles = files ?? await getAllPlayList()
   return await readPlayList(playListFiles)
