d6866c53452170f049d3eb5d41d31ed043fdd2a4
He110te4m <i@he110.info>
2024-02-09 16:40:43 +0800

    refactor: update server

--- netlify/functions/redirect.ts
+++ netlify/functions/redirect.ts
@@ -1,5 +1,6 @@
 import { resolve } from 'node:path'
 import { existsSync } from 'node:fs'
+import { readFile } from 'node:fs/promises'
 import express, { Router } from 'express'
 import serverless from 'serverless-http'
 import { getToday } from '../../src/date'
@@ -7,13 +8,15 @@ import { getToday } from '../../src/date'
 const api = express()
 
 const router = Router()
-router.get('/hello', (req, res) => {
+router.get('/hello', async (req, res) => {
   let file = resolve(`./dist/${getToday()}.m3u8`)
   if (!existsSync(file)) {
     file = resolve('./dist/iptv.m3u8')
   }
 
-  res.sendFile(file)
+  const buff = await readFile(file)
+  res.set('Content-Disposition', 'iptv.m3u8')
+  res.end(buff)
 })
 
 api.use('/api/', router)
