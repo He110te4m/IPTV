3cd6c277d73e6090a141da3b208f0ae12aaa9606
He110te4m <i@he110.info>
2024-02-09 19:01:25 +0800

    remove old date

--- netlify/functions/source/chaozhou.ts
+++ netlify/functions/source/chaozhou.ts
@@ -1,10 +1,6 @@
 export const chaozhouSource = `
 #EXTM3U
 
-#EXTINF:-1 tvg-name="汕头综合" tvg-logo="https://epg.112114.xyz/logo/汕头综合.png" group-title="地方",汕头综合
-https://sttv-tsl.cutv.com/lKGXIQa/500/l3sgKgX.m3u8
-#EXTINF:-1 tvg-name="汕头经济" tvg-logo="https://epg.112114.xyz/logo/汕头经济.png" group-title="地方",汕头经济
-http://211.97.63.13:801/hls/82/index.m3u8
 #EXTINF:-1 tvg-name="潮州综合" tvg-logo="https://epg.112114.xyz/logo/潮州综合.png" group-title="地方",潮州综合
 http://time-vod-qhlive.sobeylive.com/czbtv2019/czzhpd.m3u8
 #EXTINF:-1 tvg-name="潮州民生" tvg-logo="https://epg.112114.xyz/logo/潮州民生.png" group-title="地方",潮州民生
