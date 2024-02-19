c12bb2ce8ccbc246b23fc8baacfac5f1a571e3cd
He110te4m <i@he110.info>
2024-02-19 10:15:30 +0800

    feat: add network sources

--- netlify/functions/source/index.ts
+++ netlify/functions/source/index.ts
@@ -1,12 +1,22 @@
 import fetch from 'node-fetch'
 import { chaozhouSource } from './chaozhou'
 
-export async function getIPTVSources() {
-  const allSources = await Promise.allSettled([
-    (await fetch('https://iptv.b2og.com/fmml_ipv6.m3u')).text(),
-  ])
+const sourceUrl = [
+  'https://iptv.b2og.com/fmml_ipv6.m3u',
+  'https://iptv.b2og.com/ycl_iptv.m3u',
+  'https://iptv.b2og.com/m_iptv.m3u',
+  'https://iptv.b2og.com/o_cn.m3u',
+  'https://iptv.b2og.com/q_bj_iptv_unicom.m3u',
+]
 
-  return [chaozhouSource].concat(
+export async function getIPTVSources() {
+  const allSources = await Promise.allSettled(
+    sourceUrl.map(async url => (await fetch(url)).text()),
+  )
+
+  return [
+    chaozhouSource,
+  ].concat(
     allSources.flatMap(item => item.status === 'fulfilled' ? item.value : []),
   )
 }
