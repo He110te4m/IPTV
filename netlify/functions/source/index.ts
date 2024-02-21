e1542f5a05fa8feb1501dd06f7400fcffa7e9c7b
He110te4m <me@he110.site>
2024-02-21 14:55:26 +0800

    refactor: 切换动态源地址

--- netlify/functions/source/index.ts
+++ netlify/functions/source/index.ts
@@ -2,11 +2,10 @@ import fetch from 'node-fetch'
 import { chaozhouSource } from './chaozhou'
 
 const sourceUrl = [
-  'https://iptv.b2og.com/fmml_ipv6.m3u',
-  'https://iptv.b2og.com/ycl_iptv.m3u',
-  'https://iptv.b2og.com/m_iptv.m3u',
-  'https://iptv.b2og.com/o_cn.m3u',
-  'https://iptv.b2og.com/q_bj_iptv_unicom.m3u',
+  'https://iptv.002397.xyz/fmml_ipv6.m3u',
+  'https://iptv.002397.xyz/ycl_iptv.m3u',
+  'https://iptv.002397.xyz/m_iptv.m3u',
+  'https://iptv.002397.xyz/q_bj_iptv_unicom.m3u',
 ]
 
 export async function getIPTVSources() {
