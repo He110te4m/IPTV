5f8ff794b9158adb7b9b3bc33e5cebc8c97e1c8e
He110te4m <me@he110.site>
2025-04-30 09:09:00 +0800

    refactor: 重新启用路径生成函数，移除注释并整理代码结构

--- netlify/functions/sources/shantou/generator.ts
+++ netlify/functions/sources/shantou/generator.ts
@@ -1,6 +1,7 @@
 import md5 from 'md5'
 import { getNow } from '../../utils/parse'
-import { pathname } from './pathname'
+
+// import { pathname } from './pathname'
 
 export function generateShantouPlaylist(date: Date, id: string) {
   const livekey = pathname(date, id)
@@ -13,54 +14,54 @@ export function generateShantouPlaylist(date: Date, id: string) {
   return `https://sttv-hls.strtv.cn/${id}/500/${livekey}.m3u8?sign=${sign}&t=${t}`
 }
 
-// function pathname(today: Date, e: string): string {
-//   const o = today.valueOf()
-//   globalThis.console.log(`o: ${o}`)
-//   let r = 0
-//   let d = -1
-//   let l = 0
-//   for (let a = 0; a < e.length; a++) {
-//     const p = e.charCodeAt(a)
-//     r += p
-//     if (d !== -1) {
-//       l += d - p
-//     }
-//     d = p
-//   }
-//   const s = (r += l).toString(36)
-//   let c = o.toString(36)
-//   let u = 0
-//   for (let a = 0; a < c.length; a++) {
-//     u += c.charCodeAt(a)
-//   }
-//   c = c.substr(5) + c.substr(0, 5)
-//   let h
-//   let v
-//   const f = Math.abs(u - r)
-//   c = s.split('').reverse().join('') + c
+function pathname(today: Date, e: string): string {
+  const o = today.valueOf()
+  globalThis.console.log(`o: ${o}`)
+  let r = 0
+  let d = -1
+  let l = 0
+  for (let a = 0; a < e.length; a++) {
+    const p = e.charCodeAt(a)
+    r += p
+    if (d !== -1) {
+      l += d - p
+    }
+    d = p
+  }
+  const s = (r += l).toString(36)
+  let c = o.toString(36)
+  let u = 0
+  for (let a = 0; a < c.length; a++) {
+    u += c.charCodeAt(a)
+  }
+  c = c.substr(5) + c.substr(0, 5)
+  let h
+  let v
+  const f = Math.abs(u - r)
+  c = s.split('').reverse().join('') + c
 
-//   const g = c.substr(0, 4)
-//   const w = c.substr(4)
-//   const m = []
-//   const b = today.getDay() % 2
+  const g = c.substr(0, 4)
+  const w = c.substr(4)
+  const m = []
+  const b = today.getDay() % 2
 
-//   for (let a = 0; a < e.length; a++) {
-//     if (a % 2 === b) {
-//       m.push(c.charAt(a % c.length))
-//     } else {
-//       h = e.charAt(a - 1)
-//       if (h) {
-//         v = g.indexOf(h)
-//         if (v === -1) {
-//           m.push(h)
-//         } else {
-//           m.push(w.charAt(v))
-//         }
-//       } else {
-//         m.push(g.charAt(a))
-//       }
-//     }
-//   }
+  for (let a = 0; a < e.length; a++) {
+    if (a % 2 === b) {
+      m.push(c.charAt(a % c.length))
+    } else {
+      h = e.charAt(a - 1)
+      if (h) {
+        v = g.indexOf(h)
+        if (v === -1) {
+          m.push(h)
+        } else {
+          m.push(w.charAt(v))
+        }
+      } else {
+        m.push(g.charAt(a))
+      }
+    }
+  }
 
-//   return ((f.toString(36).split('').reverse().join('')) + m.join('')).substr(0, e.length)
-// }
+  return ((f.toString(36).split('').reverse().join('')) + m.join('')).substr(0, e.length)
+}
