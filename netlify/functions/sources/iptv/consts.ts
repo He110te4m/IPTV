8ca6f7cb4f4fff423a7fc84c8a63b47ee2bd63ef
He110te4m <me@he110.site>
2025-04-30 15:09:33 +0800

    feat: 添加香港和CCTV源文件，更新播放列表生成逻辑

--- netlify/functions/sources/iptv/consts.ts
+++ netlify/functions/sources/iptv/consts.ts
@@ -3,6 +3,9 @@ export const sourceUrl = [
   // 'https://iptv.002397.xyz/ycl_iptv.m3u',
   // 'https://iptv.002397.xyz/m_iptv.m3u',
   // 'https://iptv.002397.xyz/q_bj_iptv_unicom.m3u',
+
+  'https://raw.githubusercontent.com/iptv-org/iptv/refs/heads/master/streams/hk.m3u',
+  'https://raw.githubusercontent.com/iptv-org/iptv/refs/heads/master/streams/cn_cctv.m3u',
 ]
 
 export const timeout = 10000
--- netlify/functions/sources/iptv/index.ts
+++ netlify/functions/sources/iptv/index.ts
@@ -2,9 +2,13 @@ import { parseM3U } from '@iptv/playlist'
 import type { Source } from '../types'
 import { sourceUrl, timeout } from './consts'
 
+// const pickChannelKeywords = ['CCTV', '广东', '湖南', '香港']
+
 export default {
   async get() {
-    return (await getIPTVSources()).flatMap(source => parseM3U(source).channels)
+    return (await getIPTVSources())
+      .flatMap(source => parseM3U(source).channels)
+      // .filter(channel => pickChannelKeywords.some(keyword => channel.name?.includes(keyword)))
   },
 } satisfies Source
 
